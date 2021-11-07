8b9abd82e80789e22be9f44dce657eb959580390
Miller777 <miller777@mail.ru>
2021-11-08 01:15:59 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(context.watch<SwitchData>().getData.toString()),
+          ),
+          body: Center(
+            child: Switch(
+              value: context.watch<SwitchData>().getData,
+
+              onChanged: (value) =>
+                  context.read<SwitchData>().changeSwitch(value),
+              //activeTrackColor: Colors.yellow,
+              //activeColor: Colors.orangeAccent,
+            ),
+          )),
+    );
+  }
+}
+
+class SwitchData with ChangeNotifier {
+  bool _data = false;
+
+  bool get getData => _data;
+
+  void changeSwitch(bool newBool) {
+    _data = newBool;
+
+    notifyListeners();
+  }
+}
